6c1e8b988cdfad910f11fd7662456641ce10dc79
masha yanson <mashayanson@gmail.com>
2023-04-20 23:03:34 +0200

    Добавлена функция сравнения плоских файлов

--- bin/gendiff.mjs
+++ bin/gendiff.mjs
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 const program = new Command();
-/* {
-option:()=>...
-parse:()=>...
-} */
+import gendiff from '../src/diff.mjs'
+
 program
-    .usage("[options]")
-    .description('Compares two configuration files and shows a difference.')
+    .version('0.1.0')
+    .usage("[options] <filepath1> <filepath2>")
     .option('-V, --version', 'output the version number')
+    .arguments('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
+    .action(gendiff)
     .parse()
-
-
-console.log(program.opts())
