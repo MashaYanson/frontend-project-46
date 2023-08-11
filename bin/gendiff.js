53895a5f56ef22a94d6af8d467ad33d25d543b86
masha yanson <mashayanson@gmail.com>
2023-08-11 15:12:41 +0200

    action added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,12 +5,13 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
+  .name('gendiff')
   .version('0.1.0')
   .usage('[options] <filepath1> <filepath2>')
   .arguments('<filepath1> <filepath2>')
   .option('-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action(genDiff);
+  .action((file1, file2) => console.log(genDiff(file1, file2, program.opts().format)));
 
 program.parse();
