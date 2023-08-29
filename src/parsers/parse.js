dcd12db01a56d9637c44822c3ad3b67ce2288ac4
masha yanson <mashayanson@gmail.com>
2023-08-29 12:13:02 +0200

    added switch case to parser

--- src/parsers/parse.js
+++ src/parsers/parse.js
@@ -1,17 +1,20 @@
 import YAML from 'yaml';
 
 const parseFile = (data, format) => {
-  if (format === '.json') {
-    return JSON.parse(data);
-  }
-  if (format === '.yml' || format === '.yaml') {
-    try {
-      return YAML.parse(data);
-    } catch (e) {
-      console.error(`Error parsing YAML file: ${data}`);
+  switch (format) {
+    case '.json':
+      return JSON.parse(data);
+
+    case '.yml' || '.yaml':
+      try {
+        return YAML.parse(data);
+      } catch (e) {
+        console.error(`Error parsing YAML file: ${data}`);
+        return {};
+      }
+
+    default:
       return {};
-    }
   }
-  return null;
 };
 export default parseFile;
