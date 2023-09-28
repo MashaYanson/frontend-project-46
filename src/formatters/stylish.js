90e07fb430523c634ffce938bd5c87b98cdd75c7
masha yanson <mashayanson@gmail.com>
2023-09-28 14:42:52 +0200

    замена на flatMap stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,21 +4,24 @@ const prefixMap = {
   added: '  + ',
   nested: '    ',
 };
+
 function indent(depth) {
   return depth ? '    '.repeat(depth) : '';
 }
+
 function stringifyValue(value, depth) {
   if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
     const entries = Object.entries(value)
-      .map(([k, v]) => `${indent(depth)}    ${k}: ${stringifyValue(v, depth + 1)}`)
+      .flatMap(([k, v]) => `${indent(depth)}    ${k}: ${stringifyValue(v, depth + 1)}`)
       .join('\n');
     return `{\n${entries}\n${indent(depth)}}`;
   }
   return value;
 }
+
 const stylish = (tree) => {
   function convertASTToString(ast, depth = 0) {
-    const lines = ast.map((node) => {
+    const lines = ast.flatMap((node) => {
       const {
         key, status, value, children, value1, value2,
       } = node;
@@ -28,8 +31,10 @@ const stylish = (tree) => {
         case 'unchanged':
           return `${indent(depth)}${prefixMap[status]}${key}: ${stringifyValue(value, depth + 1)}`;
         case 'changed':
-          // eslint-disable-next-line max-len
-          return `${indent(depth)}${prefixMap.deleted}${key}: ${stringifyValue(value1, depth + 1)}\n${indent(depth)}${prefixMap.added}${key}: ${stringifyValue(value2, depth + 1)}`;
+          return [
+            `${indent(depth)}${prefixMap.deleted}${key}: ${stringifyValue(value1, depth + 1)}`,
+            `${indent(depth)}${prefixMap.added}${key}: ${stringifyValue(value2, depth + 1)}`,
+          ];
 
         case 'nested':
           return `${indent(depth)}${prefixMap[status]}${key}: ${convertASTToString(children, depth + 1)}`;
@@ -42,4 +47,5 @@ const stylish = (tree) => {
 
   return convertASTToString(tree);
 };
+
 export default stylish;
